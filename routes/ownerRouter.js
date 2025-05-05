8edb6f652431e70c2fc9f537ad4d312887d83e38
Harsh Tanwar <harsh.tanwar9818@gmail.com>
2025-05-05 21:40:49 +0530

    ejs pages

--- routes/ownerRouter.js
+++ routes/ownerRouter.js
@@ -2,15 +2,31 @@ const express = require('express');
 const router = express.Router();
 const ownerModel = require('../models/owner-model');
 
+if(process.env.NODE_ENV === "development"){
+    router.post("/create", async (req,res) => {
+        let owner = await ownerModel.find();
+        if(owner.length > 0){
+            return res
+                .status(503)
+                .send("you don't have permission to create a new owner.");
+        }
+
+        let{fullname, email, password} = req.body;
+
+        let createdOwner = await ownerModel.create({
+            fullname,
+            email,
+            password,
+        });
+
+        res.status(201).send(createdOwner);
+    });
+}
+
+
 router.get('/', (req, res) => {
     res.send('Owner Home Page');
 });
-
-if(process.env.NODE_ENV === "development"){
-    router.post("/create", (req,res) => {
-        res.send("Create route working");
-    });
-}
 
 
 module.exports = router;   
